f453f9617611e9488dfd0df82c9c02fe0cccfd89
RafaelGGlo <rafg79505@gmail.com>
2026-06-13 14:33:52 -0300

    Implementação variáveis de ambiente

--- src/serve.js
+++ src/serve.js
@@ -1,3 +1,4 @@
+import 'dotenv/config';
 import express from "express";
 import mysql from "mysql2/promise";
 
@@ -7,11 +8,11 @@ const app = express();
 app.use(express.json());
 
 const pool = mysql.createPool({
-    host: "localhost",
-    user:"root",
-    password: "1234",
-    database: "user_db",
-    port: 3306,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    port: process.env.DB_PORT,
     wintForConnections: true,
     connectionLimit: 10,
     queueLimit:0
@@ -45,7 +46,7 @@ app.post('/user', async (req, res) => {
             [nome, email, cpf, apelido]
         );
     }
-    catch(Erro){
+    catch(error){
         console.error("Erro ao criar usuário");
         res.status(201).json({msg:"Usuario Criador com sucesso"});
     }
